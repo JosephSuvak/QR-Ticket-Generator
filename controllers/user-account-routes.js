dce3bdeecd2c71b282ddf1f1170ce860f01627ec
walterego <walter.scimone@gmail.com>
2021-05-20 16:33:29 -0400

    user route concert update

--- controllers/user-account-routes.js
+++ controllers/user-account-routes.js
@@ -5,27 +5,31 @@ const { User, Ticket, Concert } = require('../../models');
 // getting all users...
 router.get('/', (req, res) => {
     User.findAll({
-      attributes: { exclude: ['password'] }
+        attributes: { exclude: ['password'] }
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 // getting users by id...
-  router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     User.findOne({
-      attributes: { exclude: ['password'] },
-      where: {
-        id: req.params.id
-      },
-      include: [
-        {
-          model: Ticket,
-          attributes: ['id', 'user_id', 'concert_id']
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
         },
-        
-      ]
+        include: [
+            {
+                model: Ticket,
+                attributes: ['id', 'user_id', 'concert_id']
+            },
+            {
+                model: Concert,
+                attributes: ['id', 'venue_name', 'concert_name']
+            }
+
+        ]
     })
 });
