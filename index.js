b66aa492fd6f0fc9908bf4d42b21d2a50b27d0ea
JPB <jbanas9124@gmail.com>
2021-05-23 11:54:22 -0400

    seed script npm run seedDev

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-//const seedAll = require('./seeds/seed-index');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -46,9 +44,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //controllers and api for handlebars
 app.use("/", require('./controllers/'));
-
-//app.use(seedAll);
-
 
 //database and server
 sequelize.sync({ force: false }).then(() => {
