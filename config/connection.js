ec59b09d3919c76af5cb83e0de338ef855bf0a65
Joseph Suvak <JosephSuvak@users.noreply.github.com>
2021-05-23 15:40:00 -0500

    Update NPM

--- config/connection.js
+++ config/connection.js
@@ -28,6 +28,7 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   client.end();
 });*/
 
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
