e885c473cad120aabe08a514d38864504d670edf
Mediogre <caseyarrington13@gmail.com>
2021-05-24 21:23:29 -0500

    started routes/logic to update routes

--- public/javascript/add_ticket.js
+++ public/javascript/add_ticket.js
@@ -22,9 +22,35 @@ async function newTicketHandler(event) {
   //if ok go to dashboard where you should see the new post
   if (response.ok) {
     document.location.replace('/account');
+    stockDecreaser();
   } else {
     alert(response.statusText);
   }
+}
+
+const stockDecreaser = () => {
+  const oldStock = document.querySelector('#stock').value;
+
+  const stock = oldStock - 1;
+
+  const concert_id = document.querySelector('input[name="singleConcertID]').value
+
+  fetch(`/api/concert/${concert_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      stock
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(ConcertDataResponse => {
+    console.log(ConcertDataResponse);
+
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 //create new post listener
 document.querySelector('#single_concert').addEventListener('click', newTicketHandler);
