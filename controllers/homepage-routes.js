51f952a628f61b6208964c4e8b93d937a6133ec5
JPB <jbanas9124@gmail.com>
2021-05-26 10:31:18 -0400

    fix to heroku login page being displayed instead of homepage

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -4,9 +4,10 @@ const sequelize = require('../config/connection');
 const { User, Ticket, Concert } = require('../models');
 const chalk = require('chalk');
 const QRCode = require('qrcode');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../utils/auth');
+const withLoggedIn = require('../utils/loggedIn');
 
-router.get('/', withAuth, (req, res) => {
+router.get('/', (req, res) => {
     console.log(req.session);
     Concert.findAll({
       attributes: [
